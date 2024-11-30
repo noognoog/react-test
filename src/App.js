78b17e31866eb5393e5f4cda6cac96a94e367d50
noognoog <noognoog@naver.com>
2024-11-30 21:26:34 +0900

    change

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import Detail from "./routes/Detail";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/hello" element={<h1>Hello</h1>}></Route>
-        <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path={"/movie/hello"} element={<h1>Hello</h1>}></Route>
+      <Route path={"/movie/:id"} element={<Detail />}></Route>
+      <Route path={"/"} element={<Home />}></Route>
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // BrowserRouter 추가
 import App from "./App";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter basename="/react-test">
+    {" "}
+    {/* basename 설정 */}
+    <App />
+  </BrowserRouter>
+);
